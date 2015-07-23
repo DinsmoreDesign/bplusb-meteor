43406a5d77dbd4f319bc13ddbf147ab046d31464
Ben Cutrell <bcutrell@dhcp-a-092.int.smartleaf.com>
2015-07-23 08:38:20 -0400

    adds simple db example

--- client/lib/bplusb-meteor.js
+++ client/lib/bplusb-meteor.js
@@ -1,30 +1,28 @@
-// Tasks = new Mongo.Collection("guests");
-// PlayersList.insert({
-//   name: text,
-//   rsvp: boolean,
-//   createdAt: new Date()
-// })
+Guests = new Mongo.Collection("guests");
 
-
+// Not sure if conditionals are necessary
 if (Meteor.isClient) {
 
   Template.bplusb.events({
     "submit .new-task": function (event) {
-      alert('submitted');
-      
+      event.preventDefault();
       var name = event.target.text.value
       var password = event.target.password.value
+
+      // === does not use type conversion
+      // == uses type conversion
+      if (password == 1) {
+        Guests.insert({
+          name: name,
+          createdAt: new Date()
+        })
+      }
 
       // Clear Form
       event.target.text.value = ""; 
       event.target.password.value = "";
-
-      return false; // Prevent default form submit
     }
   });
-
-
-
 }
 
 if (Meteor.isServer) {
