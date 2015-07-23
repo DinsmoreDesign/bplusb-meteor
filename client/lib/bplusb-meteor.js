6d9a14d72c7633fd927001b837ca14a1892b7037
Ben Cutrell <bcutrell@dhcp-a-092.int.smartleaf.com>
2015-07-23 08:08:41 -0400

    adds vendor files

--- client/lib/bplusb-meteor.js
+++ client/lib/bplusb-meteor.js
@@ -1,4 +1,29 @@
+// Tasks = new Mongo.Collection("guests");
+// PlayersList.insert({
+//   name: text,
+//   rsvp: boolean,
+//   createdAt: new Date()
+// })
+
+
 if (Meteor.isClient) {
+
+  Template.bplusb.events({
+    "submit .new-task": function (event) {
+      alert('submitted');
+      
+      var name = event.target.text.value
+      var password = event.target.password.value
+
+      // Clear Form
+      event.target.text.value = ""; 
+      event.target.password.value = "";
+
+      return false; // Prevent default form submit
+    }
+  });
+
+
 
 }
 
