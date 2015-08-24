7409538c4eb83267e821d1681440d4267b9c4fb0
Ben Cutrell <bcutrell@dhcp-a-092.int.smartleaf.com>
2015-08-24 18:43:45 -0400

    adds validation methods

--- client/lib/bplusb-meteor.js
+++ client/lib/bplusb-meteor.js
@@ -7,6 +7,9 @@ if (Meteor.isClient) {
     "submit .new-task": function (event) {
       event.preventDefault();
 
+      $('.flash-error').hide();
+      $('.flash-error').html('');
+
       Meteor.call('sendLogMessage');
 
       var password = event.target.password.value 
@@ -18,22 +21,56 @@ if (Meteor.isClient) {
       
       var opts = {
         'numGuests': numGuests,
-        'name': names,
+        'names': names,
         'password' : password,
         'note' : noteText,
         'diet' : dietText,
         'attend' : willAttend
       }
-      // add form validations
-      Meteor.call('insertRsvpData', opts, function(error, result) {
-        if (result == true) {
-          alert('Sucess');
-        } else {
-          alert('Fail');
-        }
-      });
 
-      $('.modal-body').html('<h1>Thanks</h1>')
-    }
+      // Num Guests
+      if ( opts.numGuests.length == 0 ) {
+        $('.numGuests-group').addClass('has-error');
+        
+        $('.flash-error').show();
+        $('.flash-error').html('Please fill out the number of guests that will be attending');
+        return null;
+
+      // Names
+      } else if ( opts.names.length == 0 ) {
+        $('.names-group').addClass('has-error');
+
+        $('.flash-error').show();
+        $('.flash-error').html('Please enter your name(s)');
+
+      // Attend
+      } else if ( opts.attend.length == 0 ) {
+        $('.attend-group').addClass('has-error');
+
+        $('.flash-error').show();
+        $('.flash-error').html('Please check if you will be attending');
+
+      // Password
+      } else if ( opts.password.length == 0 ) {
+        $('.password-group').addClass('has-error');
+
+        $('.flash-error').show();
+        $('.flash-error').html('Please enter the password');
+      
+      } else {
+
+        // add form validations
+        Meteor.call('insertRsvpData', opts, function(error, result) {
+          if (result == true) {
+            $('.modal-body').html('')
+            $('.flash-good').html('Success! See you there!')
+          } else {
+            $('.flash-error').show();
+            $('.flash-error').html("Incorrect password. If you are having trouble, please email bcutrell13@gmail.com (it's probably my fault");
+          }
+        });
+      }
+    } // end event method
+
   });
 }
